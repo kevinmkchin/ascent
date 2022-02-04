3561e59e51ae180e1748b43770f12e146dd91d56
stakikawa <suzuran.takikawa@gmail.com>
2022-02-03 20:09:56 -0800

    Implement basic enemy action

--- src/ai_system.cpp
+++ src/ai_system.cpp
@@ -1,7 +1,18 @@
 // internal
 #include "ai_system.hpp"
 
+static float elapsedTime = 0.0f;
+
 void AISystem::step(float deltaTime)
 {
+	elapsedTime += deltaTime * 1000.0f;
+	if (elapsedTime >= 2000.0f) {
 
+		for (Entity& enemy : registry.enemy.entities) {
+			Motion& enemyMotion = registry.motions.get(enemy);
+			enemyMotion.velocity.x *= -1.f;
+		}
+		elapsedTime = 0.0f;
+
+	}
 }
